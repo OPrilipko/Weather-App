92b66918359541d5c26621aa5a702cc1b8447af3
OPrilipko <84148336+OPrilipko@users.noreply.github.com>
2021-05-13 20:03:42 -0400

    Uploaded Weather App Files to VS

---
+++ src/index.js
@@ -0,0 +1,44 @@
+function showTemperature(response) {
+  console.log(response.data);
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#degrees").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+}
+
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-city-input").value;
+  let apiKey = "e81420198cdd89cc0dae2cc13b1f4f88";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+let form = document.querySelector("#search-form");
+
+form.addEventListener("submit", search);
+
+let now = new Date();
+let li = document.querySelector("li");
+
+let date = now.getDate();
+let year = now.getFullYear();
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+let currentMonth = months[now.getMonth()];
+
+li.innerHTML = `${currentMonth} ${date}, ${year}`;
