268d07db40ded3a643fae2638cbc288ba0b4f46e
OPrilipko <84148336+OPrilipko@users.noreply.github.com>
2021-05-23 12:46:59 -0400

    updated search engine and created function for current location

--- src/index.js
+++ src/index.js
@@ -19,6 +19,21 @@ function search(event) {
 
   axios.get(apiUrl).then(showTemperature);
 }
+
+function searchLocation(position) {
+  let apiKey = "e81420198cdd89cc0dae2cc13b1f4f88";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
 let form = document.querySelector("#search-form");
 
 form.addEventListener("submit", search);
